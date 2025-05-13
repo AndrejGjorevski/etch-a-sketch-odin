ae04bd8d04ffafd2c4da385e4a6c48fae19c8385
AndrejGjorevski <gorevskia9@gmail.com>
2025-05-14 01:14:58 +0200

    Add eventListener for coloring the boxes

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ const container = document.getElementById("container");
 for (let i = 0; i < 256; i++) {
     container.appendChild(document.createElement("div"));
 }
+
+const board = container.querySelectorAll("div");
+board.forEach(box => 
+    box.addEventListener("mouseenter", (e) => e.target.style.backgroundColor = "black")
+)
