88c015f472535a74a7041a20011e0525d4fa9a07
AndrejGjorevski <gorevskia9@gmail.com>
2025-05-14 01:11:46 +0200

    Revert "Add eventListener for coloring the boxes"
    This reverts commit 42e0b13769931dd750e113680e96455927e1775f.

--- script.js
+++ script.js
@@ -2,12 +2,3 @@ const container = document.getElementById("container");
 for (let i = 0; i < 256; i++) {
     container.appendChild(document.createElement("div"));
 }
-
-function colorBox(event) {
-    event.target.style.backgroundColor = "black";
-}
-
-const board = container.querySelectorAll("div");
-board.forEach(box => {
-    box.addEventListener("mouseenter", colorBox);
-});
