9d956fd8db8398cc052ca6b72d580cc973c31bd6
AndrejGjorevski <gorevskia9@gmail.com>
2025-05-14 02:01:11 +0200

    Add function for creating a new board and reformat script.js code

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
+const button = document.getElementById("btn");
 const container = document.getElementById("container");
-for (let i = 0; i < 256; i++) {
-    container.appendChild(document.createElement("div"));
+
+function createGrid(n) {
+    container.innerHTML = "";
+
+    let boxSize = 960 / n;
+    for (let i = 0; i < n * n; i++) {
+        let div = document.createElement("div");
+        div.style.width = boxSize + "px";
+        div.style.height = boxSize + "px";
+        container.appendChild(div);
+        div.addEventListener("mouseenter", (e) => e.target.style.backgroundColor = "black");
+    }
 }
 
-const board = container.querySelectorAll("div");
-board.forEach(box => 
-    box.addEventListener("mouseenter", (e) => e.target.style.backgroundColor = "black")
-)
+createGrid(16);
+
+button.addEventListener("click", (e) => {
+    n = prompt("How many boxes per row (less than 100): ");
+    n = parseInt(n);
+    if (isNaN(n) || n <= 0 || n > 100) {
+        alert("Please enter a valid number between 0 and 100");
+        return;
+    }
+
+    createGrid(n);
+});
+
